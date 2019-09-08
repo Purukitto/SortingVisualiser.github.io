6bf6f342699cc196ef2e1beab1886c5ed8a8176e
Purukitto <purukitto@gmail.com>
2019-09-09 00:32:43 +0530

    Selection with c

--- algorithms/selection_sort.js
+++ algorithms/selection_sort.js
@@ -2,39 +2,58 @@ let values = [];
 let i = 0;
 let j = 0;
 let w = 10;
+let states = [];
 
 function setup() {
     createCanvas(windowWidth - 16, windowHeight - 65);
     values = new Array(floor(width / w));
     for (let i = 0; i < values.length; i++) {
         values[i] = random(height);
+        states[i] = -1;
     }
-    frameRate(15);
+    selectionSort(values, values.length);
+}
+
+async function selectionSort(arr, n) {
+    var min_idx;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        states[min_idx] = 0;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                states[min_idx] = -1;
+                states[j] = 0;
+                min_idx = j;
+            }
+        }
+        await swap(arr, min_idx, i);
+        states[min_idx] = -1;
+    }
 }
 
 function draw() {
     background(51);
-    if (i < values.length - 1) {
-        var min = i;
-        for (var j = i; j < values.length; j++) {
-            if (values[j] < values[min]) min = j;
-        }
-        swap(values, i, min);
-    } else {
-        console.log("finished");
-        noLoop();
-    }
-    i++;
 
     for (let i = 0; i < values.length; i++) {
         noStroke();
-        fill(255);
+        if (states[i] == 0) {
+            fill('#d23941');
+        } else if (states[i] == 1) {
+            fill('#39d2ca');
+        } else {
+            fill(255);
+        }
         rect(i * w, height - values[i], w, values[i]);
     }
 }
 
-function swap(arr, a, b) {
+async function swap(arr, a, b) {
+    await sleep(5);
     let temp = arr[a];
     arr[a] = arr[b];
     arr[b] = temp;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
--- algorithms/test.js
+++ algorithms/test.js
@@ -2,56 +2,39 @@ let values = [];
 let i = 0;
 let j = 0;
 let w = 10;
-let states = [];
 
 function setup() {
     createCanvas(windowWidth - 16, windowHeight - 65);
     values = new Array(floor(width / w));
     for (let i = 0; i < values.length; i++) {
         values[i] = random(height);
-        states[i] = -1;
     }
-    insertionSort(values, values.length);
-}
-
-async function insertionSort(arr, n) {
-    if (n <= 1)
-        return;
-    await insertionSort(arr, n - 1);
-    var last = arr[n - 1];
-    j = n - 2;
-    while (j >= 0 && arr[j] > last) {
-        states[j] = 0;
-        states[last] = 0;
-        await move(arr, j, last);
-        states[j] = -1;
-        states[last] = -1;
-        j--;
-    }
-    arr[j + 1] = last;
+    frameRate(15);
 }
 
 function draw() {
     background(51);
+    if (i < values.length - 1) {
+        var min = i;
+        for (var j = i; j < values.length; j++) {
+            if (values[j] < values[min]) min = j;
+        }
+        swap(values, i, min);
+    } else {
+        console.log("finished");
+        noLoop();
+    }
+    i++;
 
     for (let i = 0; i < values.length; i++) {
         noStroke();
-        if (states[i] == 0) {
-            fill('#d23941');
-        } else if (states[i] == 1) {
-            fill('#39d2ca');
-        } else {
-            fill(255);
-        }
+        fill(255);
         rect(i * w, height - values[i], w, values[i]);
     }
 }
 
-async function move(arr, j, last) {
-    await sleep(5);
-    arr[j + 1] = arr[j];
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function swap(arr, a, b) {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 }
